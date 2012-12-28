99000a2d2677a37ae4a8628d1bc3329927133218
Jason Nochlin <hundredwatt@gmail.com>
2012-12-28 18:39:45 -0500

    Rails 3.0.17 app uses same secret token and session name as other test apps

--- test/rails3017/config/initializers/secret_token.rb
+++ test/rails3017/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3017::Application.config.secret_token = '819e4c3429ab5dcb2a0c84241ba89db33798ad3b7928b53000792c4ae5cb38633c8b78a990f4aa6c48a9172e46d95bb1cff88bb6ae52ed1eaec411ee68e4e516'
+Rails3017::Application.config.secret_token = '123456789012345678901234567890'
--- test/rails3017/config/initializers/session_store.rb
+++ test/rails3017/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails3017::Application.config.session_store :cookie_store, :key => '_rails3017_session'
+Rails3017::Application.config.session_store :cookie_store, :key => '_flashpatch_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
