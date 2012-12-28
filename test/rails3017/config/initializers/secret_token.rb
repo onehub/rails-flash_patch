fd41eb140671e0d9d6dd7b5d3857632768fce402
Jason Nochlin <hundredwatt@gmail.com>
2012-12-28 18:38:57 -0500

    Generate a Rails 3.0.17 app

---
+++ test/rails3017/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3017::Application.config.secret_token = '819e4c3429ab5dcb2a0c84241ba89db33798ad3b7928b53000792c4ae5cb38633c8b78a990f4aa6c48a9172e46d95bb1cff88bb6ae52ed1eaec411ee68e4e516'
