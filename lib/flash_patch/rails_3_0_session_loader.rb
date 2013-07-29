464bc3648d4cc8d3c7e903bf4b2dacf60161f9f7
Leigh Caplan <lcaplan@onehub.com>
2013-07-29 14:59:52 -0700

    Don't gsub, because the whole dumped object may not be readable.

--- lib/flash_patch/rails_3_0_session_loader.rb
+++ lib/flash_patch/rails_3_0_session_loader.rb
@@ -5,7 +5,14 @@ module FlashPatch
       # class in the Marshal-dumped string. So, we can call the backport
       # class FlashGash and use gsub so that Marshal will load the Rails 3.0 session
       # hash using it
-      session = Marshal.load decrypted_session_string.gsub('FlashHash','FlashGash')
+      idx   = decrypted_session_string.index('FlashHash')
+      g_idx = idx + 5
+
+      cleaned_string = decrypted_session_string.dup
+      cleaned_string[g_idx] = 'G'
+
+      session = Marshal.load cleaned_string
+
       flash_messages_from_original_klass = session.delete('flash')
       session['flash'] = ActionDispatch::Flash::FlashHash.new.update(flash_messages_from_original_klass)
       session
