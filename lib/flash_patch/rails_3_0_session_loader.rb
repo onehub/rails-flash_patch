2abc7e7c7e7a86401f87391a719fcb4650905141
Leigh Caplan <lcaplan@onehub.com>
2013-07-29 15:31:03 -0700

    Only set the session flash if there was one in the first place.

--- lib/flash_patch/rails_3_0_session_loader.rb
+++ lib/flash_patch/rails_3_0_session_loader.rb
@@ -17,7 +17,7 @@ module FlashPatch
       session = Marshal.load decrypted_session_string
 
       flash_messages_from_original_klass = session.delete('flash')
-      session['flash'] = ActionDispatch::Flash::FlashHash.new.update(flash_messages_from_original_klass)
+      session['flash'] = ActionDispatch::Flash::FlashHash.new.update(flash_messages_from_original_klass) if flash_messages_from_original_klass
       session
     end
   end
